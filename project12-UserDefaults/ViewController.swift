5fa6e69f0195c41e85d51bac5af7f7b5ba35e555
Lydia <guipopo5@gmail.com>
2024-03-06 15:42:16 +0800

    OK

--- project12-UserDefaults/ViewController.swift
+++ project12-UserDefaults/ViewController.swift
@@ -11,9 +11,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let defaults = UserDefaults.standard
+        
+        defaults.set(25, forKey: "Age")
+        defaults.set(true, forKey: "UseFaceID")
+        defaults.set(CGFloat.pi, forKey: "Pi")
+        
+        defaults.set("Paul Hudson", forKey: "Name")
+        defaults.set(Data(), forKey: "LastRun")
+        
+        let array = ["Hello", "World"]
+        defaults.set(array, forKey: "SavedArray")
+        
+        let dict = ["Name": "Paul", "Country": "UK"]
+        defaults.set(dict, forKey: "SavedDictionary")
+            
+        let sevedInteger = defaults.integer(forKey: "Age")
+        let sevedBoolean = defaults.bool(forKey: "UserFaceID")
+        
+        let sevedArray = defaults.object(forKey: "SevedArray") as? [String] ?? [String]()
+        let sevedDictionary = defaults.object(forKey: "SevsdDictionary") as? [String: String] ?? [String: String] ()
+        
+//        let sevedArray2 = defaults.array(forKey: sevedDictionary)
     }
-
-
 }
 
